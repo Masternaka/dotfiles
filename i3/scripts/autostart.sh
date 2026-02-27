3234b2d75eaec9c1400238a745caa464481d1f1e
Masternaka <gabthesniper@gmail.com>
2026-02-27 13:14:08 -0500

    update autostart

--- i3/scripts/autostart.sh
+++ i3/scripts/autostart.sh
@@ -9,7 +9,7 @@ sleep 1
 # Active le picom (compositor)
 killall picom
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
-picom --config ~/.config/i3/picom/picom.conf --animations -b &
+picom --config ~/.config/picom/picom.conf --vsync &
 
 # Active Polybar (barre de statut)
 ~/.config/i3/polybar/polybar-i3 &
